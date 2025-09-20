225d6af077414b6527f090ab53af1ba75fed1128
mohdsajid9600 <mohdsajid9600@gmail.com>
2025-09-20 19:07:51 +0530

    Add computerchoice function to computer move.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,6 @@
  const button = document.querySelectorAll('.btn');
         let userMove;
+        // Result Function
         button.forEach(btn => {
             btn.addEventListener('click', () => {
                 userMove = btn.textContent;
@@ -8,4 +9,16 @@
                       Result is ${showResult()}`);
             });
         });
-       
+       // Computer Choice Function 
+        function computerChoice() {
+           let ComputerChoseRanNum = Math.floor(Math.random() * 3);
+            console.log(ComputerChoseRanNum);
+            if (ComputerChoseRanNum === 0) {
+                return "Bat";
+            } else if (ComputerChoseRanNum === 1) {
+                return "Ball";
+            } else {
+                return "Stump";
+            }
+        }
+        
