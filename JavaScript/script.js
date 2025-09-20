18c87e4feeeb68a792091df172556555d689ca79
mohdsajid9600 <mohdsajid9600@gmail.com>
2025-09-20 22:23:14 +0530

    Refactor CSS to unify button styles and update HTML structure for reset button; enhance score tracking logic in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,23 +1,5 @@
- const button = document.querySelectorAll('.btn');
+        const button = document.querySelectorAll('.btn');
         let userMove;
-        let score ={
-            win:0,
-            lose:0,
-            tie:0
-        };
-
-        // Computer Choice Function 
-        function computerChoice() {
-           let ComputerChoseRanNum = Math.floor(Math.random() * 3);
-            if (ComputerChoseRanNum === 0) {
-                return "Bat";
-            } else if (ComputerChoseRanNum === 1) {
-                return "Ball";
-            } else {
-                return "Stump";
-            }
-        }
-
         let ComputerMove;
 
         // Display Function
@@ -41,13 +23,48 @@
 
                 // Display Score on the Screen on each screen
                 let userScore = document.querySelector('#show-score');
-                userScore.textContent = `Score:  Wins ${score.win} , Losses ${score.lose} , Ties ${score.tie}`;    
+                userScore.textContent = score.displayScore();    
             });
         });
 
+        const resetBtn = document.querySelector('.reset-btn');
+        resetBtn.addEventListener('click', () => {
+            localStorage.clear().resetScore(localScore);
+        
+        });
+
+        // Score Keeping using Local Storage
+
+        let localScore = localStorage.getItem('score');
+        let score;
+        // Reset Score Function
+        function resetScore(localScore) {
+            score = localScore ? JSON.parse(localScore) :
+        {
+            win:0,
+            lose:0,
+            tie:0
+        };}
+
+        score.displayScore = function() {
+        return `Score:Won:${score.win}, Lost:${score.lost}, Tie: ${score.tie}`;
+      };
+        
+        // Computer Choice Function 
+        function computerChoice() {
+           let ComputerChoseRanNum = Math.floor(Math.random() * 3);
+            if (ComputerChoseRanNum === 0) {
+                return "Bat";
+            } else if (ComputerChoseRanNum === 1) {
+                return "Ball";
+            } else {
+                return "Stump";
+            }
+        }
+
         // Game Result Function
         function gameResult() {
-
+            localStorage.setItem('score', JSON.stringify(score));
             if (userMove === ComputerMove) {
                 score.tie++;
                 return "It's a Tie!";
