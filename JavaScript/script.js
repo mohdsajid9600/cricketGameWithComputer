5b2c845e85095bc4e7fa49d97e5a9ea8f0eee789
mohdsajid9600 <mohdsajid9600@gmail.com>
2025-09-20 21:37:49 +0530

    Update index.html and script.js to display user and computer choices, game results, and score on the screen

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,7 +4,8 @@
             win:0,
             lose:0,
             tie:0
-        }
+        };
+
         // Computer Choice Function 
         function computerChoice() {
            let ComputerChoseRanNum = Math.floor(Math.random() * 3);
@@ -18,26 +19,35 @@
         }
 
         let ComputerMove;
-        
+
         // Display Function
         button.forEach(btn => {
             btn.addEventListener('click', () => {
                 userMove = btn.textContent;
                 ComputerMove = computerChoice();
-                console.log(`From event listner `,userMove);
-                console.log(`From event listner `,ComputerMove);
-                alert(`You have to chose ${btn.textContent} and Computer chose ${ComputerMove}.
-                
-                      Result is ${gameResult()}
-                     
-                      ${score.win} Wins, ${score.lose} Losses, ${score.tie} Ties`
-                    );
+
+                // Display Choices and Score on the Screen
+                let userChoice = document.querySelector('#user-choice');
+                userChoice.textContent = `You Chose: ${btn.textContent}`;
+
+                // Display Computer Choice on the Screen
+                let computerChoiceDisplay = document.querySelector('#computer-choice');
+                computerChoiceDisplay.textContent = `Computer Chose: ${ComputerMove}`;
+
+                // Display Result on the Screen on each screen
+                let showResult = document.querySelector('#show-result');
+                showResult.textContent = gameResult();
+
+
+                // Display Score on the Screen on each screen
+                let userScore = document.querySelector('#show-score');
+                userScore.textContent = `Score:  Wins ${score.win} , Losses ${score.lose} , Ties ${score.tie}`;    
             });
         });
 
         // Game Result Function
         function gameResult() {
-         console.log(`From gameresult funtion `, userMove, ComputerMove);
+
             if (userMove === ComputerMove) {
                 score.tie++;
                 return "It's a Tie!";
